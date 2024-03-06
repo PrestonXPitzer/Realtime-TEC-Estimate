7f325599166257213b3a504151770fe4565df967
PrestonXPitzer <prestonpitzer20@vt.edu>
2024-03-06 15:00:50 -0500

    2

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #read the contentens of out.txt
-    with open('out.txt', 'r') as file:
-        data = file.read()
+    with open('out.txt', 'r') as f:
+        data = f.read()
+    f.close()
     return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
